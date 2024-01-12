21bcd11694282a9d236a72507a5f370a6fde1b7a
kahigamadeit <kahingakimani@gmail.com>
2024-01-12 01:17:23 -0800

    klk

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -1,16 +1,22 @@
 #!/usr/bin/python3
 from sys import argv
+
 def arguments():
     arg_string = "arguments"
     c_or_d = "."
     length = len(argv) - 1
-    if (length == 1):
+
+    if length == 1:
         arg_string = "argument"
-    if (length - 1 >= 0):
+    if length - 1 >= 0:
         c_or_d = ":"
-    print("{} {}{}".format(length, arg_string, c_or_d))
+
+    print(f"{length} {arg_string}{c_or_d}")
+
     for index, arg in enumerate(argv):
-        if (index > 0):
-            print("{}: {}".format(index, arg))
+        if index > 0:
+            print(f"{index}: {arg}")
+
 if __name__ == "__main__":
     arguments()
+
